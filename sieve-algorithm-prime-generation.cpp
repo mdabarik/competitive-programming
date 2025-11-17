68bb363ecaa9f2bec8e029000fc7e7ccd7576bb6
Md. A. Barik <mdabarik19@gmail.com>
2025-11-17 17:30:18 +0600

    Update sieve-algorithm-prime-generation.cpp

--- sieve-algorithm-prime-generation.cpp
+++ sieve-algorithm-prime-generation.cpp
@@ -1,17 +1,28 @@
-int mx = 1e7;
-vector<bool> prime(mx+123);
+int mx = 3*1e3 + 123;
+vector<bool> prime(mx+123, true);
+vector<int> primeNum;
+
 void primeGeneration() {
-  for (int i=3; i<=mx; i+=2) {
-    prime[i] = true;
-  }
-  
-  prime[2] = true;
-  int sq = sqrt(mx);
-  for (int i=3; i<=sq; i+=2) {
-    if (prime[i]) {
-      for (int j=i*i; j<=mx; j+=i) {
-        prime[j] = false;
-      }
+    prime[0] = prime[1] = false;
+
+    // Remove even numbers (except 2)
+    for (int i = 4; i <= mx; i += 2) {
+        prime[i] = false;
     }
-  }
+
+    int sq = sqrt(mx);
+    for (int i = 3; i <= sq; i += 2) {
+        if (prime[i]) {
+            // j starts from i*i and jumps 2*i (only odd multiples)
+            for (int j = i * i; j <= mx; j += 2 * i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    // Collect primes (skip evens)
+    primeNum.push_back(2);
+    for (int i = 3; i <= mx; i += 2) {
+        if (prime[i]) primeNum.push_back(i);
+    }
 }
