d1bd124ac60a24ca25ba08177db77f71e1f65a2c
Md. A. Barik <mdabarik19@gmail.com>
2025-11-19 01:12:31 +0600

    Create CF-Template.cpp

---
+++ CF-Template.cpp
@@ -0,0 +1,36 @@
+
+/* Bismillah hir Rohmanir Rohim */
+/* La Hawala Wa Lakuata Illah Billah */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+// #define int long long int
+#define coutBit(x)  (int)__builtin_popcount(x)
+#define lsb(x)  (int)__builtin_ctzll(x)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl '\n'
+#define PI acos(-1.0)
+#define lcm(a,b) (a/__gcd(a, b)*b)
+
+/* Robbi zidni Ilma */
+void BarikErSolution() {
+	int n; cin>>n;
+	if (n%2) yes;
+	else no;
+}
+
+signed main() {
+	ios::sync_with_stdio(false); 
+	cin.tie(nullptr);
+
+	/* amdr main logic */
+	int testcase; 
+	cin>>testcase;
+	while (testcase--) {
+		BarikErSolution();
+	}
+	
+	return 0;
+}
